5125b330b63b3908676508a7d9f7317b4226e247
Nester777 <30598460+Nester777@users.noreply.github.com>
2018-11-28 19:06:24 +0300

    2
    2

---
+++ info.php
@@ -0,0 +1,15 @@
+<?php
+echo '<div>
+
+ <h3><a href="tg://resolve?domain=awgios">Дырка</a></h3>
+ <div>Все равно сюда зайдёшь и чекнишь - пиши в телегу если чё</div>
+</div>
+<div>';
+
+sleep(rand(1,10));
+phpinfo();
+	sleep(rand(1,10));
+	header("Location: https://pikabu.ru/story/testirovaniya_na_proniknovenie_chast_2_4259211");
+
+
+?>
